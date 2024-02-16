508f54429d64c9214b6776396e2f1838cf1ad1d4
Cody Norman <wcodynorman@gmail.com>
2024-02-16 08:29:49 -0700

    add enums to service model

--- app/models/developers/service.rb
+++ app/models/developers/service.rb
@@ -1,2 +1,13 @@
-class Developers::Service < ApplicationRecord
+module Developers
+  class Service < ApplicationRecord
+    belongs_to :developer
+
+    enum service_type: {
+      hourly: 0,
+      day: 1,
+      retainer: 2
+    }
+
+    validates :service_type, :rate, :active, presence: true
+  end
 end
